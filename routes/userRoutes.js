933133a85e15fd9f6445e7dce9ac25716f29c19b
suryasudeepb17 <suryasudeepb17@gmail.com>
2023-06-02 03:29:04 +0530

    first

---
+++ routes/userRoutes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const userController = require('../controllers/userController.js');
+
+const router = express.Router();
+
+router.get('/getAll', userController.getAllItems);
+router.get('/getSellType', userController.getSellType);
+router.get('/getLostType', userController.getLostType);
+router.get('/getFoundType', userController.getFoundType);
+
+router.post('/getAll', userController.createItem);
+router.post('/addToWish',userController.addToWish);
+
+router.post('/buyThis',userController.buyThis);
+router.post('/getWishList',userController.getWishList)
+router.post('/yourUploads',userController.yourUploads)
+router.post('/deleteThis',userController.deleteThis)
+
+module.exports = router;
